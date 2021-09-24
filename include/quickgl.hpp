b34332b47e2639c0973859a64a7501387f370241
Igor Nunes <thoga31@gmail.com>
2021-09-24 17:33:46 +0100

    Adds extendable qgl_callback namespace
    - Callback methods are now pointers to functions;
    - The Scene instance bings itself to the qgl_callback namespace;
    - The window title is now a property of the Scene instance.

--- include/quickgl.hpp
+++ include/quickgl.hpp
@@ -1,11 +1,21 @@
 #ifndef QUICKGL_H
 #define QUICKGL_H
 
-#include <glad/glad.h>
-#define GLM_FORCE_RADIANS
+#ifdef QGL_GLAD
+    #include <glad/glad.h>
+    #define GLM_FORCE_RADIANS
+#else
+    #ifdef QGL_GLAD_LOCAL
+        #include "glad/glad.h"
+        #define QGL_GLAD
+        #define GLM_FORCE_RADIANS
+    #endif
+#endif
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+
+#include <string>
 
 #include "camera.hpp"
 
@@ -14,12 +24,16 @@
     #include FT_FREETYPE_H
 #endif
 
+using namespace std;
+
 class Scene {
 private:
     bool success = false;
     GLFWwindow* window;
+    
     unsigned scr_height;
     unsigned scr_width;
+    string   scr_title;
 
     /* Timing */
     float deltaTime = 0.0f;
@@ -28,36 +42,59 @@ private:
 
     Camera camera;
 
+    GLFWframebuffersizefun framebuffer_size_callback = nullptr;
+    GLFWmousebuttonfun     mousebtn_callback         = nullptr;
+    GLFWcursorposfun       mouse_callback            = nullptr;
+    GLFWscrollfun          scroll_callback           = nullptr;
+
     bool init_glfw(void);
+    #ifdef QGL_GLAD
     int init_glad(void);
+    #endif
 
 protected:
     const unsigned DEFAULT_SCR_HEIGHT = 800;
     const unsigned DEFAULT_SCR_WIDTH  = 600;
+    const string   DEFAULT_SCR_TITLE  = "";
+
+    /* Callbacks */
+
+public:
+    /* Initialization, render cycle and finalization */
+    void initialize(void);
+    void initialize(const unsigned, const unsigned);
+    void initialize(const unsigned, const unsigned, string);
+    void finalize(void);
+    bool launchSuccessful(void);
+    void run(void);
+    virtual void refresh(void);
+
+    /* Properties and fields */
+    GLFWwindow* getWindow(void);
 
     /* Callbacks */
     void setFrameBufferSizeCallback(GLFWframebuffersizefun);
     void setMouseButtonCallback(GLFWmousebuttonfun);
     void setCursorPositionCallback(GLFWcursorposfun);
     void setScrollCallback(GLFWscrollfun);
-
-public:
-    /* Initialization, render cycle and finalization */
-    void initialize(void);
-    void initialize(const unsigned, const unsigned);
-    void finalize(void);
-    bool launchSuccessful(void);
-    void run(void);
-    void refresh(void);
-
-    /* Properties and fields */
-    GLFWwindow* getWindow(void);
-
-    /* Callbacks */
-    virtual void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-    virtual void mousebtn_callback(GLFWwindow *window, int button, int action, int mods);
-    virtual void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-    virtual void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 };
+
+
+/* === NAMESPACE qgl_callback ===
+ * Namespace to be extended with callback functions that might need to refer to
+ * the Scene instance.
+ * At each iteration, the Scene will bind itself to this namespace.
+ * */
+namespace qgl_callback {
+    Scene* scene;
+
+    void bindInstance(Scene *scn) {
+        scene = scn;
+    }
+
+    Scene* getInstance(void) {
+        return scene;
+    }
+}
 
 #endif
--- src/quickgl.cpp
+++ src/quickgl.cpp
@@ -1,12 +1,10 @@
 #include "quickgl.hpp"
 
-
-
 GLFWwindow* Scene::getWindow(void) {
     return this->window;
 }
 
-bool Scene::init_glfw(int width, int height, const char* title) {
+bool Scene::init_glfw(void) {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -16,7 +14,7 @@ bool Scene::init_glfw(int width, int height, const char* title) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
     
-    this->window = glfwCreateWindow(width, height, title, NULL, NULL);
+    this->window = glfwCreateWindow(this->scr_width, this->scr_height, this->scr_title.c_str(), NULL, NULL);
     if (this->window == NULL)
         return false;
     
@@ -32,21 +30,38 @@ bool Scene::init_glfw(int width, int height, const char* title) {
     return true;
 }
 
+#ifdef QGL_GLAD
 int Scene::init_glad(void) {
     return gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 }
+#endif
 
 
 
 void Scene::initialize(void) {
-    this->initialize(DEFAULT_SCR_HEIGHT, DEFAULT_SCR_WIDTH);
+    this->initialize(DEFAULT_SCR_HEIGHT, DEFAULT_SCR_WIDTH, DEFAULT_SCR_TITLE);
 }
 
-void Scene::initialize(unsigned height, unsigned width) {
+void Scene::initialize(const unsigned width, const unsigned height) {
+    this->initialize(width, height, DEFAULT_SCR_TITLE);
+}
+
+void Scene::initialize(const unsigned width, const unsigned height, string title) {
+    this->success = true;
+
     this->scr_height = height;
     this->scr_width  = width;
+    this->scr_title  = title;
 
-    this->success = true;
+    if (!this->init_glfw()) {
+        this->success = false;
+    }
+
+    #ifdef QGL_GLAD
+    if (!this->init_glad()) {
+        this->success = false;
+    }
+    #endif
 }
 
 bool Scene::launchSuccessful(void) {
@@ -55,6 +70,7 @@ bool Scene::launchSuccessful(void) {
 
 void Scene::run(void) {
     while (!glfwWindowShouldClose(this->getWindow())) {
+        qgl_callback::bindInstance(this);
         this->refresh();
     }
 }
@@ -64,7 +80,7 @@ void Scene::finalize(void) {
 }
 
 
-
+/*
 void Scene::setFrameBufferSizeCallback(GLFWframebuffersizefun callback) {
     glfwSetFramebufferSizeCallback(this->window, callback);
 }
@@ -80,3 +96,4 @@ void Scene::setCursorPositionCallback(GLFWcursorposfun callback) {
 void Scene::setScrollCallback(GLFWscrollfun callback) {
     glfwSetScrollCallback(this->window, callback);
 }
+*/
